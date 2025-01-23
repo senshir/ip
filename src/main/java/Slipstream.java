6ce9f6a0d067b84f8ab1de0a7cdf7ed7bfa45b6f
senshir <shishirsen2004@gmail.com>
2025-01-24 04:25:53 +0800

    Changed Slipstream's function from responding to what I say to adding tasks to a list

--- src/main/java/Slipstream.java
+++ src/main/java/Slipstream.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Slipstream {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Slipstream\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + "____________________________________________________________\n";
         System.out.println(logo);
 
         // Initialise a scanner to read user's input
         Scanner sc = new Scanner(System.in);
+
+        // Create a list of tasks
+        ArrayList<String> tasks = new ArrayList<>();
 
         while (true) {
             // Read the user's input
@@ -23,11 +25,19 @@ public class Slipstream {
                         + " Bye. Hope to see you again soon!\n"
                         + "____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                // Show all the tasks in the list
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                // Repeat the user's input
-                System.out.println("____________________________________________________________\n"
-                        + " " + input + "\n"
-                        + "____________________________________________________________");
+                // Add the user's input to the list
+                tasks.add(input);
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
     }
